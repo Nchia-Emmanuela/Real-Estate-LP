4fa9e0ab36908c19eb72387452d0ac0c369da7c6
unknown <ekeinchiaemmanuela@gmail.com>
2022-05-06 02:34:53 -0700

    nav bar responsiveness

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict'
+const menu = document.getElementsByClassName('fa-bars')[0]
+
+menu.addEventListener('click', () => {
+    document.querySelector('#list').classList.remove('control__menu');
+    if(document.querySelector('#list').classList.contains('show')) {
+        document.querySelector('#list').classList.add('hide');
+        document.querySelector('#list').classList.remove('show');
+        return;
+    }
+    document.querySelector('#list').classList.toggle('show');
+    document.querySelector('#list').classList.remove('hide');
+})
+
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 838) {
+        document.querySelector('#list').classList.add('control__menu');
+        document.querySelector('#list').classList.remove('hide');
+        document.querySelector('#list').classList.remove('show');
+    }
+});
+
+// console.log(menu, ul)
